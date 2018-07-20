29030d855557ab68e88059866ce56b5c60e69089
mbwatson <mattwat@gmail.com>
2018-07-19 22:42:38 -0400

    abstract author-building

--- djoi/utils.py
+++ djoi/utils.py
@@ -1,17 +1,27 @@
 from djoi.authors.models import Author, Alias
 from djoi.publications.models import Publication
 
+def findKnownAuthor(author):
+    first_name = author['given']
+    last_name = author['family']
+    known_author = Author.objects.by_name(first_name, last_name).first() or None
+    known_alias = Alias.objects.filter(name=f'{first_name} {last_name}').first() or None
+    if known_author:
+        return known_author
+    elif known_alias:
+        return known_alias.author
+    else:
+        return {
+            'first_name': author['given'],
+            'last_name': author['family'],
+            'full_name': f'{author["given"]} {author["family"]}',
+        }
+
 def getAuthors(authors):
     author_objects = []
-    wanted_keys = ['given', 'family']
     for author in authors:
-        alias = Alias.objects.filter(name=f'{author["given"]} {author["family"]}').first() or None
-        if alias:
-            author_objects.append(alias.author)
-        else:
-            known_author = Author.objects.by_name(author['given'], author['family']).first() or None
-            if known_author:
-                author_objects.append(known_author)
+        known_author = findKnownAuthor(author)
+        author_objects.append(known_author)
     return author_objects
 
 def publicationObject(work):

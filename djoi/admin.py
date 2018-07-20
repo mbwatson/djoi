9e1f94a1558554bd50edc7bb2e5dec4d75b0cd29
mbwatson <mattwat@gmail.com>
2018-07-20 17:15:26 -0400

    exclude editing title and authors

--- djoi/admin.py
+++ djoi/admin.py
@@ -17,6 +17,7 @@ admin.site.register(Author, AuthorAdmin)
 
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ('doi', 'title')
+    exclude = ('author', 'title')
 
     def save_related(self, request, form, formsets, change):
         super(PublicationAdmin, self).save_related(request, form, formsets, change)

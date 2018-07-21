de5f0ee8cc01b39424d3941ea81e22c43c7b421e
mbwatson <mattwat@gmail.com>
2018-07-21 12:17:56 -0400

    show citation when editing publication

--- djoi/admin.py
+++ djoi/admin.py
@@ -18,7 +18,8 @@ admin.site.register(Author, AuthorAdmin)
 
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ('doi', 'title')
-    exclude = ('author', 'title', 'citation')
+    # exclude = ('author', 'title', 'citation')
+    exclude = ('author', 'title',)
     
     def save_related(self, request, form, formsets, change):
         super(PublicationAdmin, self).save_related(request, form, formsets, change)

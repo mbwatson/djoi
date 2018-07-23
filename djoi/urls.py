553a81d384fbf60ce2aa90ff3070aa6f17a00f92
mbwatson <mattwat@gmail.com>
2018-07-23 10:47:22 -0400

    reorganize routing

--- djoi/urls.py
+++ djoi/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+
+from . import views
 import djoi.publications.views as publicationViews
-import djoi.authors.views as authorViews
 import djoi.staff.views as staffViews
 
 app_name = 'djoi'
 
 urlpatterns = [
-    path('authors', authorViews.index, name='authors'),
-    path('authors/<slug:author_slug>/', authorViews.detail, name='author'),
-
+    path('', views.index, name='index'),
     path('publications', publicationViews.publications, name='publications'),
-
     path('staff', staffViews.index, name='staff'),
     path('staff/<slug:employee_slug>/', staffViews.detail, name='employee'),
 ]
